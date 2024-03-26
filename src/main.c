07a25276d4a4aeef4bc4d4cb20a0d9cd22454cca
Kameron <kameronmpenn@gmail.com>
2024-03-26 15:05:09 -0400

    oops

--- src/main.c
+++ src/main.c
@@ -1676,13 +1676,12 @@ void draw_chat_box()
                 {
                     if(memcmp("$", chat_text, 1) == 0)
                     {
-                        is_cmd = true;
                         char* argv[20] = {0};
                         int argc = 0;
 
                         for(int i = 0; i < 20; ++i)
                         {
-                            char* s = string_split_index_copy(msg+1, " ", i, true);
+                            char* s = string_split_index_copy(chat_text+1, " ", i, true);
                             if(!s) break;
                             argv[argc++] = s;
                         }
